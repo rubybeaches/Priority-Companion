823f5386049faeb35cc8c14b0546115803ccd87d
rubybeaches <patrick.mclain.jr@gmail.com>
2023-11-21 11:12:08 -0600

    commenting out count until db is setup

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ const dancer = Dancing_Script({ subsets: ["latin"] });
 const Page = async () => {
   const { habits, initiatives } = fetchRoleData(Roles[0].id);
 
-  const habitCount = await getHabitByRoleCount(1);
+  /* const habitCount = await getHabitByRoleCount(1);*/
 
   return (
     <main className="card">
@@ -47,7 +47,8 @@ const Page = async () => {
               <p>{role.description}</p>
               <div>
                 <h3 className="roleHeader">
-                  Habits <span className="roleHeaderCount">({habitCount})</span>
+                  Habits{" "}
+                  {/* <span className="roleHeaderCount">({habitCount})</span>*/}
                 </h3>
                 {habits.map((habit) => (
                   <span key={habit.id}>
@@ -100,9 +101,7 @@ const Page = async () => {
               <h2 className="roleHeader">{role.name}</h2>
               <p>{role.description}</p>
               <div>
-                <h3 className="roleHeader">
-                  Habits <span className="roleHeaderCount">({habitCount})</span>
-                </h3>
+                <h3 className="roleHeader">Habits</h3>
                 {habits.map((habit) => (
                   <span key={habit.id}>
                     <Task
@@ -154,9 +153,7 @@ const Page = async () => {
               <h2 className="roleHeader">{role.name}</h2>
               <p>{role.description}</p>
               <div>
-                <h3 className="roleHeader">
-                  Habits <span className="roleHeaderCount">({habitCount})</span>
-                </h3>
+                <h3 className="roleHeader">Habits</h3>
                 {habits.map((habit) => (
                   <span key={habit.id}>
                     <Task
@@ -208,9 +205,7 @@ const Page = async () => {
               <h2 className="roleHeader">{role.name}</h2>
               <p>{role.description}</p>
               <div>
-                <h3 className="roleHeader">
-                  Habits <span className="roleHeaderCount">({habitCount})</span>
-                </h3>
+                <h3 className="roleHeader">Habits</h3>
                 {habits.map((habit) => (
                   <span key={habit.id}>
                     <Task
