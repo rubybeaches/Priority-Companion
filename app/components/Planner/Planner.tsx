c20cc216cfbb302a7a38d6542ba75dd0b5a203a3
rubybeaches <patrick.mclain.jr@gmail.com>
2024-02-14 15:46:21 -0600

    undo comment

--- app/components/Planner/Planner.tsx
+++ app/components/Planner/Planner.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 
 const Planner = async ({ pathname }: { pathname: string }) => {
   const tasks = await getTasks();
-  console.log(tasks);
   return (
     <div className="sidebar" id="planner">
       <div style={{ position: "relative", width: "40%" }}>
