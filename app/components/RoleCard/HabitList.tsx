7a949888b6569416a1b434a152dd3627d75cf05d
rubybeaches <patrick.mclain.jr@gmail.com>
2024-02-17 00:50:48 -0600

    fixing a few style issues

--- app/components/RoleCard/HabitList.tsx
+++ app/components/RoleCard/HabitList.tsx
@@ -1,17 +1,13 @@
 "use client";
-import { useState } from "react";
 import { Role } from "@/app/lib/definitions";
-import { Task as TaskProp } from "@prisma/client";
 import Task from "../Task/Task";
 
 const HabitList = ({ Role }: { Role: Role }) => {
-  const [tasks, setTasks] = useState(() =>
-    Role.habits.map((habit) => {
-      if (habit.tasks.length > 0 && habit.tasks[0].completed == false) {
-        return habit.tasks[0];
-      }
-    })
-  );
+  const tasks = Role.habits.map((habit) => {
+    if (habit.tasks.length > 0 && habit.tasks[0].completed == false) {
+      return habit.tasks[0];
+    }
+  });
 
   return (
     <>
