06b1b17674c03b19a679adb0248a185fb7a03aec
rubybeaches <patrick.mclain.jr@gmail.com>
2024-02-17 14:19:04 -0600

    further tests of mobile scale and date picker

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  width: "700px",
-  initialScale: 1.0,
+  width: "700",
+  initialScale: 1,
   userScalable: true,
 };
 
